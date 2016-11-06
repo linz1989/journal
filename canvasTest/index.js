2fb1c5fed6942a2eca298e713216ec13bf73a756
zhangjialin <jialin2013@qq.com>
2016-11-06 18:59:27 +0800

    33

--- canvasTest/index.js
+++ canvasTest/index.js
@@ -1,11 +1,12 @@
 $(function(){
     drawPreQrCodeImg();
 
-    var currStyle = 1;
-    var rectStyleCtx = $("#rectStyle")[0].getContext("2d");
-    var heartStyleCtx = $("#heartStyle")[0].getContext("2d");
-    var currCanvas = $("#rectStyle")[0];
+    var currStyle = 5;
+    var rectStyleCanvas = $("#rectStyle")[0];
+    var heartStyleCanvas = $("#heartStyle")[0];
+    var currCanvas;
     var currCtx;
+    var tempQrCode = $('#tempQrCode');
 
     $("#testBtn").click(function(){
         var zip = new JSZip();
@@ -15,7 +16,9 @@ $(function(){
             {serialNo : "3344444", qrCodeData : "hdhdh"},
             {serialNo : "445566", qrCodeData : "xncbcb"}];
         var imgData, item;
-        currCtx = currStyle !=5 ? rectStyleCtx : heartStyleCtx;
+        currCanvas = currStyle !=5 ? rectStyleCanvas : heartStyleCanvas;
+        currCtx = currCanvas.getContext("2d");
+
         for(var k=0;k<testData.length;k++){
             item = testData[k];
             imgData = drawCodeStyle(item.serialNo,item.qrCodeData);
@@ -23,8 +26,6 @@ $(function(){
             zip.file(item.serialNo+".png",imgData,{ base64 : true });
             console.log("complete："+k+"--"+item.serialNo);
         }
-        //imgData  = $("#c1")[0].toDataURL("image/png");
-        //console.log(imgData);
         zip.generateAsync({type:"blob"}).then(function (blob) {
             saveAs(blob, "imgs.zip");
         });
@@ -35,6 +36,18 @@ $(function(){
         var h = currCanvas.height;
         currCtx.clearRect(0,0,w,h);
         var copyCanvas;
+
+        currCtx.textAlign = "center";
+        currCtx.textBaseline = "middle";
+
+        tempQrCode.html("");
+        tempQrCode.qrcode({
+            width:390,
+            height:390,
+            text:qrCodeData,
+            roundBlank:false
+        });
+
         if(currStyle == 1){
             copyCanvas = $("#c1")[0];
             currCtx.drawImage(copyCanvas,0,0);
@@ -42,20 +55,55 @@ $(function(){
             ///绘制工号
             currCtx.fillStyle = "#575757";
             currCtx.font = "normal 42px 微软雅黑";
-            currCtx.textAlign = "center";
-            currCtx.textBaseline = "middle";
             currCtx.fillText(serialNo,w/2,290);
 
-            $('#tempQrCode').html("");
-            $('#tempQrCode').qrcode({
-                width:390,
-                height:390,
-                text:qrCodeData,
-                roundBlank:false
-            });
+            currCtx.drawImage(tempQrCode[0].querySelector("canvas"),(w-390)/2,350,390,390);
+        }
+        else if(currStyle == 2){
+            copyCanvas = $("#c2")[0];
+            currCtx.drawImage(copyCanvas,0,0);
 
-            var tempCode = $('#tempQrCode>canvas')[0];
-            currCtx.drawImage(tempCode,(w-390)/2,350,390,390);
+            //////绘制工号
+            currCtx.fillStyle = "#fff";
+            currCtx.font = "italic 42px 微软雅黑";
+             var rectW = Math.floor(currCtx.measureText(serialNo).width)+16;
+            currCtx.fillRect((w-rectW)/2,237,rectW+10,50);
+            currCtx.fillStyle = "#e40080";
+            currCtx.fillText(serialNo,w/2,262);
+
+            currCtx.drawImage(tempQrCode[0].querySelector("canvas"),(w-390)/2,353,390,390);
+        }
+        else if(currStyle == 3){
+            copyCanvas = $("#c3")[0];
+            currCtx.drawImage(copyCanvas,0,0);
+            ////绘制工号
+            currCtx.fillStyle = "#52bca8";
+            currCtx.font = "normal 48px 微软雅黑";
+            currCtx.fillText(serialNo,w/2,h-298);
+
+            currCtx.drawImage(tempQrCode[0].querySelector("canvas"),0,0,390,390,(w-324)/2,300,324,324);
+        }
+        else if(currStyle == 4){
+            copyCanvas = $("#c4")[0];
+            currCtx.drawImage(copyCanvas,0,0);
+
+            ////绘制工号
+            currCtx.fillStyle = "#9e272b";
+            currCtx.font = "normal 48px 微软雅黑";
+            currCtx.fillText(serialNo,w/2,h-275);
+
+            currCtx.drawImage(tempQrCode[0].querySelector("canvas"),(w-390)/2,280,390,390);
+        }
+        else if(currStyle == 5){
+            copyCanvas = $("#c5")[0];
+            currCtx.drawImage(copyCanvas,0,0);
+
+            ////绘制工号
+            currCtx.fillStyle = "rgb(235,200,71)";
+            currCtx.font = "bold 80px 微软雅黑";
+            currCtx.fillText(serialNo,w/2,h-183);
+
+            currCtx.drawImage(tempQrCode[0].querySelector("canvas"),(w-400)/2,238,400,400);
         }
         return currCanvas.toDataURL("image/png")
     }
@@ -66,13 +114,14 @@ $(function(){
         drawSecondCode(w,h);
         drawThirdCode(w,h);
         drawFourthCode(w,h);
+        drawFifthCode();
     }
 
     function drawFirstCode(w,h){ ////绘制样式一
         var c1 = $("#c1")[0].getContext("2d");
-        c1.strokeStyle = "#c5c5c5";
-        c1.lineWidth = 3;
-        c1.strokeRect(0,0,w,h);
+
+        c1.fillStyle="#fff";
+        c1.fillRect(0,0,w,h);
 
         c1.beginPath();
         c1.fillStyle = "#eee";
@@ -134,6 +183,10 @@ $(function(){
          c1.textAlign = "center";
          c1.textBaseline = "middle";
          c1.fillText("小摩豆健康养生会所",w/2,h-141);
+
+        c1.strokeStyle = "#c5c5c5";
+        c1.lineWidth = 3;
+        c1.strokeRect(0,0,w,h);
     }
 
     function drawSecondCode(w,h){
@@ -157,31 +210,11 @@ $(function(){
             c2.stroke();
 
             /////会所名称
-            /*c2.fillStyle = "#5C5C5C";
+            c2.fillStyle = "#5C5C5C";
              c2.font = "normal 32px 微软雅黑";
              c2.textAlign = "center";
              c2.textBaseline = "middle";
              c2.fillText("小摩豆健康养生会所",w/2,h-94);
-
-             //////绘制工号
-             var text = "006号";
-             c2.fillStyle = "#fff";
-             c2.font = "italic 42px 微软雅黑";
-             var rectW = Math.floor(c2.measureText(text).width)+16;
-             c2.fillRect((w-rectW)/2,237,rectW+10,50);
-             c2.fillStyle = "#e40080";
-             c2.fillText(text,w/2,262);
-
-             ////绘制二维码
-             $('#tempQrCode').qrcode({
-             width:390,
-             height:390,
-             text:"http://2321321321/abcde",
-             roundBlank:false
-             });
-
-             var tempCode = $('#tempQrCode>canvas')[0];
-             c2.drawImage(tempCode,(w-390)/2,353,390,390);*/
         }
     }
 
@@ -198,28 +231,50 @@ $(function(){
                 c3.drawImage(img2,(w-400)/2,108);
             }
 
-            /////会所名称
-            /*c3.fillStyle = "#6a6a6a";
-             c3.font = "normal 25px 微软雅黑";
-             c3.textAlign = "center";
-             c3.textBaseline = "middle";
-             c3.fillText("小摩豆健康养生会所", w / 2, h -135);
+            c3.strokeStyle = "#52bca8";
+            c3.lineWidth = 3;
+            c3.lineCap = "round";
 
-             ////绘制工号
-             c3.fillStyle = "#52bca8";
-             c3.font = "normal 48px 微软雅黑";
-             c3.fillText("006号",w/2,h-298);
+            var left = 119, right = w-left, height = 398, top = 264, subWidth = 75, bottom = top+height;
+            c3.beginPath();
+            c3.moveTo(left,top+subWidth);
+            c3.lineTo(left,top+subWidth-51);
+            c3.arcTo(left,top,left+subWidth-51,top,20);
+            c3.lineTo(left+subWidth,top);
 
-             ////绘制二维码
-             $('#tempQrCode').qrcode({
-             width:390,
-             height:390,
-             text:"http://2321321321/abcde",
-             roundBlank:false
-             });
+            c3.moveTo(right,top+subWidth);
+            c3.lineTo(right,top+subWidth-51);
+            c3.arcTo(right,top,right-subWidth+51,top,20);
+            c3.lineTo(right-subWidth,top);
 
-             var tempCode = $('#tempQrCode>canvas')[0];
-             c3.drawImage(tempCode,0,0,390,390,(w-324)/2,300,324,324);*/
+            c3.moveTo(left,bottom-subWidth);
+            c3.lineTo(left,bottom-subWidth+51);
+            c3.arcTo(left,bottom,left+subWidth-51,bottom,20);
+            c3.lineTo(left+subWidth,bottom);
+
+            c3.moveTo(right,bottom-subWidth);
+            c3.lineTo(right,bottom-subWidth+51);
+            c3.arcTo(right,bottom,right-subWidth+51,bottom,20);
+            c3.lineTo(right-subWidth,bottom);
+
+            left = 92;
+            right = w-92;
+            subWidth = 40;
+            top = top+height/2;
+
+            c3.moveTo(left,top);
+            c3.lineTo(left+subWidth,top);
+            c3.moveTo(right,top);
+            c3.lineTo(right-subWidth,top);
+
+            c3.stroke();
+
+            //会所名称
+            c3.fillStyle = "#6a6a6a";
+            c3.font = "normal 25px 微软雅黑";
+            c3.textAlign = "center";
+            c3.textBaseline = "middle";
+            c3.fillText("小摩豆健康养生会所", w / 2, h -135);
         }
     }
 
@@ -242,22 +297,15 @@ $(function(){
             c4.textAlign = "center";
             c4.textBaseline = "middle";
             c4.fillText("小摩豆健康养生会所", w / 2, h - 148);
+        }
+    }
 
-            ////绘制工号
-            c4.fillStyle = "#9e272b";
-            c4.font = "normal 48px 微软雅黑";
-            c4.fillText("006号",w/2,h-275);
-
-            ////绘制二维码
-            $('#tempQrCode').qrcode({
-                width:390,
-                height:390,
-                text:"http://2321321321/abcde",
-                roundBlank:false
-            });
-
-            var tempCode = $('#tempQrCode>canvas')[0];
-            c4.drawImage(tempCode,(w-390)/2,280,390,390);
+    function drawFifthCode(){
+        var c5 = $("#c5")[0].getContext("2d");
+        var img1 = new Image();
+        img1.src = "tech/5-1.png";
+        img1.onload = function(){
+            c5.drawImage(img1, 0, 0);
         }
     }
 });
